acea2877e92bd4461fe01d730c8403735c67f72b
Bllee <icpchief@gmail.com>
2018-03-12 14:16:19 -0700

    Added results table

--- app.py
+++ app.py
@@ -4,65 +4,46 @@
 ##To Do List
 ##Fix refresh issue so old data clears
 ##Fix url grab to handle all picture formats (ignoring javescript pages for now because: demo!)
+##User entry error handling.
 ##Import parsing code with model and configure (I'm dreading this)
 ##Enhance UI so parsed photos return in table format with:  site url, image url, image thumbnail, image caption
 
 from flask import Flask, render_template, flash, request, url_for, redirect
-from wtforms import Form, StringField, TextField, TextAreaField, SubmitField, validators
+#from wtforms import Form, StringField, TextField, TextAreaField, SubmitField, validators (for future refinements)
 import requests
 from bs4 import BeautifulSoup
-import webbrowser
 import urllib.parse
-###To do:  put results in image grid; parse pictures for nonpro stuff
+
 app = Flask(__name__)
-
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
 		errors = []
 		images =[]
-		try:
-			if request.method == 'POST':
-				urls = request.form['urls'].split(",")
-				for url in urls:
-					r = requests.get(url)
-					data = r.text
-					soup = BeautifulSoup(data, "html5lib")
-					for img in soup.findAll('img'):
-						src = img.get('src')
-						src = urllib.parse.urljoin(url, src) #used to get full path when sites give relative paths.
-						images.append(src)
-			return redirect(url_for('index'), errors=errors, results=images)
-				#print(images)print(images) #for debugging only
-		except:
-			errors.append("Invalid URL.  Please try again.")
-		return render_template('index.html', errors=errors, results=images)
+		if request.method == 'POST':
+			urls = request.form['urls'].split(",")
+			for url in urls:
+				r = requests.get(url)
+				data = r.text
+				soup = BeautifulSoup(data, "html5lib")
+				for img in soup.findAll('img'):
+					src = img.get('src')
+					src = urllib.parse.urljoin(url, src) #used to get full path when sites give relative paths.
+					images.append(src)
+			return render_template('parser.html', results=images, sites=urls)
+		return render_template('index.html')
+	
 
 
 @app.route('/about')
 def about():
 	return render_template('about.html')
-
-#Not sure this is needed anymore
-'''class SubmissionForm(Form):
-	site = StringField('URL', [validators.URL(require_tld=True, message="Please enter a valid URL.")])
-
-@app.route('/parser', methods=['GET','POST'])
-def parseForm():
-	form = SubmissionForm(request.form)
-	if request.method == 'POST' and form.validate():
-		return render_template('parser.html', form=form)
-	else: 
-		return render_template('index.html')
-	if request.method == 'POST':
-		site = request.form['website']
-		return render_template('success.html')
 	
-	return render_template('index.html')'''
+@app.route('/parser')
+def parser():
+	return render_template('parser.html')
 
 	
-		
 
 	
 
